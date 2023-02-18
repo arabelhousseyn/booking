5ef7257a1d7c742be8fa5f9dfb48d726d19814e0
“arabelhousseyn” <hocine.arab1@hotmail.com>
2023-02-18 19:39:26 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,7 +17,7 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        $this->user = User::factory()->create(['email' => 'test@gmail.com']);
+        $this->user = User::factory()->create(['email' => 'test12@gmail.com']);
 
         $this->seed(DatabaseSeeder::class);
     }
