18503e3f3fdf3c06dfc3745a16b4266a8c4d8602
“arabelhousseyn” <hocine.arab1@hotmail.com>
2023-04-13 00:04:56 +0100

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,9 +34,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(IdeHelperServiceProvider::class);
-        }
 
         $this->mixins();
 
